7bd042b85270803831d8e093fb74c8af9d2f457d
Huy Phan <huy.phan2210@outlook.com>
2023-04-18 15:02:31 +0700

    Update Main

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,9 +1,23 @@
 import './Main.css'
+import Account from './Account/Account';
+import OverviewCard from './OverviewCard/OverviewCard';
 
+import accountData from '../../data/accounts.json';
+import overviewData from '../../data/overview.json';
 function Main() {
 
   return (
-    <main></main>
+    <main>
+      <section className='accounts'>
+        {accountData.map((account, index) => <Account key={index} img={account.img} name={account.name} numOfChange={account.numOfChange} numOfFollowers={account.numOfFollowers} isIncreased={account.isIncreased} typeOfSocialMedia={account.typeOfSocialMedia} />)}
+      </section>
+      <section className='overview'>
+        <h1>Overview - Today</h1>
+        <div className='overview-analytics'>
+          {overviewData.map((overview, index) => <OverviewCard key={index} img={overview.img} title={overview.title} changePercent={overview.changePercent} isIncreased={overview.isIncreased} number={overview.number} />)}
+        </div>
+      </section>
+    </main>
   )
 }
 
